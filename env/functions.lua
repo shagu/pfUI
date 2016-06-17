a2c8d47db243dba41b34aa93f43a9958bfe9ce94
Eric Mauser <ericmauser@googlemail.com>
2016-06-17 23:32:55 +0200

    add round function

---
+++ env/functions.lua
@@ -0,0 +1,9 @@
+-- a real "round" instead of ceil or floor 
+function round(input, places)
+  if not places then places = 0 end
+  if type(input) == "number" and type(places) == "number" then
+    local pow = 1
+    for i = 1, places do pow = pow * 10 end
+    return floor(input * pow + 0.5) / pow
+  end
+end
