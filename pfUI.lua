286b8e6022463e2196647d6969808bf156679c4f
shagu <ericmauser@googlemail.com>
2016-12-31 18:12:13 +0100

    api: fix indentation

--- pfUI.lua
+++ pfUI.lua
@@ -99,9 +99,9 @@ message = function (msg)
 end
 
 ScriptErrors:SetScript("OnShow", function(msg)
-    DEFAULT_CHAT_FRAME:AddMessage("|cffcc3333ERROR: |cffff5555"..ScriptErrors_Message:GetText())
-    ScriptErrors:Hide()
-  end)
+  DEFAULT_CHAT_FRAME:AddMessage("|cffcc3333ERROR: |cffff5555"..ScriptErrors_Message:GetText())
+  ScriptErrors:Hide()
+end)
 
 pfUI.firstrun = CreateFrame("Frame", "pfFirstRunWizard", UIParent)
 pfUI.firstrun.steps = {}
