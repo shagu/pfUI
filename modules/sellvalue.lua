06ea4da5477d259de021782101df49a0337285d5
aixiu <4815563@qq.com>
2017-01-17 14:22:18 +0800

    更新到1.5版本

--- modules/sellvalue.lua
+++ modules/sellvalue.lua
@@ -26,15 +26,15 @@ pfUI:RegisterModule("sellvalue", function ()
           GameTooltip:AddLine(" ")
 
           if count > 1 then
-            GameTooltip:AddDoubleLine("Sell:", pfUI.api.CreateGoldString(sell) .. "|cff555555  //  " .. pfUI.api.CreateGoldString(sell*count), 1, 1, 1);
+            GameTooltip:AddDoubleLine("卖价:", pfUI.api.CreateGoldString(sell) .. "|cff555555  //  " .. pfUI.api.CreateGoldString(sell*count), 1, 1, 1);
           else
-            GameTooltip:AddDoubleLine("Sell:", pfUI.api.CreateGoldString(sell * count), 1, 1, 1);
+            GameTooltip:AddDoubleLine("卖价:", pfUI.api.CreateGoldString(sell * count), 1, 1, 1);
           end
 
           if count > 1 then
-            GameTooltip:AddDoubleLine("Buy:", pfUI.api.CreateGoldString(buy) .. "|cff555555  //  " .. pfUI.api.CreateGoldString(buy*count), 1, 1, 1);
+            GameTooltip:AddDoubleLine("购买价:", pfUI.api.CreateGoldString(buy) .. "|cff555555  //  " .. pfUI.api.CreateGoldString(buy*count), 1, 1, 1);
           else
-            GameTooltip:AddDoubleLine("Buy:", pfUI.api.CreateGoldString(buy), 1, 1, 1);
+            GameTooltip:AddDoubleLine("购买价:", pfUI.api.CreateGoldString(buy), 1, 1, 1);
           end
         end
         GameTooltip:Show()
