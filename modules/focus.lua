3f067b7ac1391c95c2a3a1b9de0cf6824c0856f6
shagu <ericmauser@googlemail.com>
2017-04-19 19:48:25 +0200

    unitframes: add focus frame configurations

--- modules/focus.lua
+++ modules/focus.lua
@@ -2,7 +2,7 @@ pfUI:RegisterModule("focus", function ()
   -- do not go further on disabled UFs
   if C.unitframes.disable == "1" then return end
 
-  pfUI.uf.focus = pfUI.uf:CreateUnitFrame("Focus", nil, nil, .2)
+  pfUI.uf.focus = pfUI.uf:CreateUnitFrame("Focus", nil, C.unitframes.focus, .2)
   pfUI.uf.focus:UpdateFrameSize()
   pfUI.uf.focus:SetPoint("CENTER", UIParent, "CENTER", 0,0)
   UpdateMovable(pfUI.uf.focus)
