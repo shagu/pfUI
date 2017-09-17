cf6521c5615fe1379c6d39be4bb858140941d736
shagu <ericmauser@googlemail.com>
2017-09-17 23:12:14 +0200

    colors: improve code formatting

--- env/colors.lua
+++ env/colors.lua
@@ -9,12 +9,12 @@ function pfUI.environment:UpdateColors()
 
   RAID_CLASS_COLORS = {
     ["WARRIOR"] = { r = 0.78, g = 0.61, b = 0.43, colorStr = "ffc79c6e" },
-    ["MAGE"] = { r = 0.41, g = 0.8, b = 0.94, colorStr = "ff69ccf0" },
-    ["ROGUE"] = { r = 1, g = 0.96, b = 0.41, colorStr = "fffff569" },
-    ["DRUID"] = { r = 1, g = 0.49, b = 0.04, colorStr = "ffff7d0a" },
-    ["HUNTER"] = { r = 0.67, g = 0.83, b = 0.45, colorStr = "ffabd473" },
-    ["SHAMAN"] = { r = 0.14, g = 0.35, b = 1.0, colorStr = "ff0070de" },
-    ["PRIEST"] = { r = 1, g = 1, b = 1, colorStr = "ffffffff" },
+    ["MAGE"]    = { r = 0.41, g = 0.8,  b = 0.94, colorStr = "ff69ccf0" },
+    ["ROGUE"]   = { r = 1,    g = 0.96, b = 0.41, colorStr = "fffff569" },
+    ["DRUID"]   = { r = 1,    g = 0.49, b = 0.04, colorStr = "ffff7d0a" },
+    ["HUNTER"]  = { r = 0.67, g = 0.83, b = 0.45, colorStr = "ffabd473" },
+    ["SHAMAN"]  = { r = 0.14, g = 0.35, b = 1.0,  colorStr = "ff0070de" },
+    ["PRIEST"]  = { r = 1,    g = 1,    b = 1,    colorStr = "ffffffff" },
     ["WARLOCK"] = { r = 0.58, g = 0.51, b = 0.79, colorStr = "ff9482c9" },
     ["PALADIN"] = { r = 0.96, g = 0.55, b = 0.73, colorStr = "fff58cba" }
   }
