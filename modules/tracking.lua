b75263bc596aecc97d859e850a99b0fb6ddf6b37
greenya <greenyadzer@gmail.com>
2017-12-05 02:58:48 +0200

    tracking: checking actual tracking spells for "pulse"

--- modules/tracking.lua
+++ modules/tracking.lua
@@ -1,5 +1,5 @@
--- todo: add dropdown with all available tracking spells on left mouse click, and "/cast" chosen spell
 pfUI:RegisterModule("tracking", function ()
+
   MiniMapTrackingFrame:UnregisterAllEvents()
   MiniMapTrackingFrame:Hide()
 
@@ -9,9 +9,37 @@ pfUI:RegisterModule("tracking", function ()
       pulse = C.appearance.minimap.tracking_pulse == "1"
   }
 
+  local knownTrackingSpellTextures = {
+    any = {
+      "Racial_Dwarf_FindTreasure", -- Find Treasure
+      "Spell_Nature_Earthquake", -- Find Minerals
+      "INV_Misc_Flower_02" -- Find Herbs
+    },
+    HUNTER = {
+      "Ability_Tracking", -- Track Beasts, 4+
+      "Spell_Holy_PrayerOfHealing", -- Track Humanoids, 10+
+      "Spell_Shadow_DarkSummoning", -- Track Undead, 18+
+      "Ability_Stealth", -- Track Hidden, 24+
+      "Spell_Frost_SummonWaterElemental", -- Track Elementals, 26+
+      "Spell_Shadow_SummonFelhunter", -- Track Deamons, 32+
+      "Ability_Racial_Avatar", -- Track Giants, 40+
+      "INV_Misc_Head_Dragon_01" -- Track Dragonkin, 50+
+    },
+    PALADIN = {
+      "Spell_Holy_SenseUndead" -- Sense Undead, 20+
+    },
+    WARLOCK = {
+      "Spell_Shadow_Metamorphosis" -- Sense Demons, 24+
+    },
+    DRUID = {
+      "Ability_Tracking" -- Track Humanoids, 32+, Cat Form only!
+    }
+  }
+
   local state = {
     texture = nil,
-    pulsing = false
+    pulsing = false,
+    spells = {}
   }
 
   pfUI.tracking = CreateFrame("Button", "pfUITracking", UIParent)
@@ -28,23 +56,34 @@ pfUI:RegisterModule("tracking", function ()
 
   pfUI.tracking:RegisterEvent("PLAYER_ENTERING_WORLD")
   pfUI.tracking:RegisterEvent("PLAYER_AURAS_CHANGED")
+  pfUI.tracking:RegisterEvent("SPELLS_CHANGED")
+  pfUI.tracking:RegisterEvent("UPDATE_SHAPESHIFT_FORMS")
   pfUI.tracking:SetScript("OnEvent", function()
-    local texture = GetTrackingTexture()
-    if texture ~= state.texture or event == "PLAYER_ENTERING_WORLD" then
-      state.texture = texture
-      if texture then
-        state.pulsing = false
-        pfUI.tracking.icon:SetTexture(texture)
-        pfUI.tracking:SetAlpha(1)
-        pfUI.tracking:Show()
-      else
-        if config.pulse then
-          state.pulsing = {tick = 1, dir = 1, min = 1, max = 25}
-          pfUI.tracking.icon:SetTexture("Interface\\Icons\\inv_misc_questionmark")
+    if event == "SPELLS_CHANGED"
+    or event == "UPDATE_SHAPESHIFT_FORMS"
+    or event == "PLAYER_ENTERING_WORLD" then
+      pfUI.tracking:RefreshSpells()
+    end
+
+    if event == "PLAYER_AURAS_CHANGED"
+    or event == "PLAYER_ENTERING_WORLD" then
+      local texture = GetTrackingTexture()
+      if texture ~= state.texture or event == "PLAYER_ENTERING_WORLD" then
+        state.texture = texture
+        if texture then
+          state.pulsing = false
+          pfUI.tracking.icon:SetTexture(texture)
+          pfUI.tracking:SetAlpha(1)
           pfUI.tracking:Show()
         else
-          state.pulsing = false
-          pfUI.tracking:Hide()
+          if config.pulse and table.getn(state.spells) > 0 then
+            state.pulsing = {tick = 1, dir = 1, min = 1, max = 25}
+            pfUI.tracking.icon:SetTexture("Interface\\Icons\\INV_Misc_QuestionMark")
+            pfUI.tracking:Show()
+          else
+            state.pulsing = false
+            pfUI.tracking:Hide()
+          end
         end
       end
     end
@@ -81,4 +120,46 @@ pfUI:RegisterModule("tracking", function ()
   pfUI.tracking:SetScript("OnLeave", function()
     GameTooltip:Hide()
   end)
+
+  function pfUI.tracking:RefreshSpells()
+    local _, playerClass = UnitClass(PLAYER)
+    local isCatForm = pfUI.tracking:PlayerIsDruidInCatForm(playerClass)
+    
+    state.spells = {}
+    for tabIndex = 1, GetNumSpellTabs() do
+      local _, _, offset, numSpells = GetSpellTabInfo(tabIndex)
+      for spellIndex = offset + 1, offset + numSpells do
+        local spellTexture = GetSpellTexture(spellIndex, BOOKTYPE_SPELL)
+        for _, c in pairs({"any", playerClass}) do
+          for _, t in pairs(knownTrackingSpellTextures[c]) do
+            if c == "DRUID" and not isCatForm then
+              break
+            end
+            if strfind(spellTexture, t) then
+              table.insert(state.spells, {
+                index = spellIndex,
+                name = GetSpellName(spellIndex, BOOKTYPE_SPELL),
+                texture = spellTexture
+              })
+              break
+            end
+          end
+        end
+      end
+    end
+  end
+
+  function pfUI.tracking:PlayerIsDruidInCatForm(playerClass)
+    if playerClass == "DRUID" then
+      for i = 0, 31 do
+        local texture = GetPlayerBuffTexture(i)
+        if not texture then break end
+        if strfind(texture, "Ability_Druid_CatForm") then
+          return true
+        end
+      end
+    end
+    return false
+  end
+
 end)
