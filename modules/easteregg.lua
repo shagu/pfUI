c08e1da445c8fdbec0a6bca9cc9f80f236d66f06
shagu <ericmauser@googlemail.com>
2017-12-23 20:00:23 +0100

    easteregg: typo fix

--- modules/easteregg.lua
+++ modules/easteregg.lua
@@ -4,8 +4,8 @@ pfUI:RegisterModule("easteregg", function ()
     local title = (UnitFactionGroup("player") == "Horde") and PVP_RANK_18_0 or PVP_RANK_18_1
 
     _G.CHAT_FLAG_AFK        = title .. " "
-    _G.MARKED_AFK           = "|cff33ffccpf|cffffffffUI|r whishes you a merry christmas, |cff33ffcc" .. title .. "|r."
-    _G.MARKED_AFK_MESSAGE   = "|cff33ffccpf|cffffffffUI|r whishes you a merry christmas, |cff33ffcc" .. title .. "|r: %s"
+    _G.MARKED_AFK           = "|cff33ffccpf|cffffffffUI|r wishes you a merry christmas, |cff33ffcc" .. title .. "|r."
+    _G.MARKED_AFK_MESSAGE   = "|cff33ffccpf|cffffffffUI|r wishes you a merry christmas, |cff33ffcc" .. title .. "|r: %s"
     _G.CLEARED_AFK          = "You are no longer |cff33ffcc" .. title .. "|r."
   end
 end)
