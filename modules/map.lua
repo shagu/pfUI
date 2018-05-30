8dd76cda2db45aa8273146536cdb16f1bc4b3efd
shagu <ericmauser@googlemail.com>
2018-05-30 21:37:18 +0200

    map: move code out of the show function

--- modules/map.lua
+++ modules/map.lua
@@ -23,62 +23,65 @@ pfUI:RegisterModule("map", function ()
 
     UIPanelWindows["WorldMapFrame"] = { area = "center" }
 
-    WorldMapFrame:SetScript('OnShow', function()
+    WorldMapFrame:SetScript("OnShow", function()
       -- default events
       UpdateMicroButtons()
       PlaySound("igQuestLogOpen")
       CloseDropDownMenus()
       SetMapToCurrentZone()
       WorldMapFrame_PingPlayerPosition()
+      LoadMovable(this)
 
       -- customize
-      WorldMapFrame:SetMovable(true)
-      WorldMapFrame:EnableMouse(true)
-      WorldMapFrame:EnableKeyboard(false)
-      WorldMapFrame:EnableMouseWheel(1)
-      WorldMapFrame:SetScript("OnMouseWheel", function()
-        if IsShiftKeyDown() then
-          c_position.alpha = pfUI.api.clamp(WorldMapFrame:GetAlpha() + arg1/10, 0.1, 1.0)
-          WorldMapFrame:SetAlpha(c_position.alpha)
-        end
-
-        if IsControlKeyDown() then
-          c_position.scale = pfUI.api.clamp(WorldMapFrame:GetScale() + arg1/10, 0.1, 2.0)
-          WorldMapFrame:SetScale(c_position.scale)
-        end
-      end)
-
-      WorldMapFrame:SetScript("OnMouseDown",function()
-        WorldMapFrame:StartMoving()
-      end)
-
-      WorldMapFrame:SetScript("OnMouseUp",function()
-        WorldMapFrame:StopMovingOrSizing()
-      end)
-
-      WorldMapFrame:ClearAllPoints()
-      WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
-      WorldMapFrame:SetWidth(WorldMapButton:GetWidth() + 15)
-      WorldMapFrame:SetHeight(WorldMapButton:GetHeight() + 55)
-
-      WorldMapFrameCloseButton:SetPoint("TOPRIGHT", WorldMapFrame, "TOPRIGHT", 0, 0)
-      CreateBackdrop(WorldMapFrame)
-
-      WorldMapFrame:SetAlpha(c_position.alpha)
-      WorldMapFrame:SetScale(c_position.scale)
-
-      BlackoutWorld:Hide()
-
-      for i,v in ipairs({WorldMapFrame:GetRegions()}) do
-         if v.SetTexture then
-           v:SetTexture("")
-         end
-
-         if v.SetText then
-           v:SetText("")
-         end
-       end
+      this:SetMovable(true)
+      this:EnableMouse(true)
+      this:EnableKeyboard(false)
+      this:EnableMouseWheel(1)
     end)
+
+    WorldMapFrame:SetScript("OnMouseWheel", function()
+      if IsShiftKeyDown() then
+        c_position.alpha = pfUI.api.clamp(WorldMapFrame:GetAlpha() + arg1/10, 0.1, 1.0)
+        WorldMapFrame:SetAlpha(c_position.alpha)
+      end
+
+      if IsControlKeyDown() then
+        c_position.scale = pfUI.api.clamp(WorldMapFrame:GetScale() + arg1/10, 0.1, 2.0)
+        WorldMapFrame:SetScale(c_position.scale)
+      end
+    end)
+
+    WorldMapFrame:SetScript("OnMouseDown",function()
+      WorldMapFrame:StartMoving()
+    end)
+
+    WorldMapFrame:SetScript("OnMouseUp",function()
+      WorldMapFrame:StopMovingOrSizing()
+      SaveMovable(this)
+    end)
+
+    WorldMapFrame:ClearAllPoints()
+    WorldMapFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+    WorldMapFrame:SetWidth(WorldMapButton:GetWidth() + 15)
+    WorldMapFrame:SetHeight(WorldMapButton:GetHeight() + 55)
+
+    WorldMapFrameCloseButton:SetPoint("TOPRIGHT", WorldMapFrame, "TOPRIGHT", 0, 0)
+    CreateBackdrop(WorldMapFrame)
+
+    WorldMapFrame:SetAlpha(c_position.alpha)
+    WorldMapFrame:SetScale(c_position.scale)
+
+    BlackoutWorld:Hide()
+
+    for i,v in ipairs({WorldMapFrame:GetRegions()}) do
+      if v.SetTexture then
+        v:SetTexture("")
+      end
+
+      if v.SetText then
+        v:SetText("")
+      end
+    end
 
     -- coordinates
     WorldMapButton.coords = CreateFrame("Frame", "pfWorldMapButtonCoords", WorldMapButton)
