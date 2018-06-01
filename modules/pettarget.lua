532a212871c49721a601c1cb34b59fa0a770ae99
shagu <ericmauser@googlemail.com>
2018-06-01 10:31:19 +0200

    pettarget: alternate position if pet is missing

--- modules/pettarget.lua
+++ modules/pettarget.lua
@@ -21,6 +21,6 @@ pfUI:RegisterModule("pettarget", function ()
 
   pfUI.uf.pettarget = pfUI.uf:CreateUnitFrame("PetTarget", nil, C.unitframes.ptarget, .2)
   pfUI.uf.pettarget:UpdateFrameSize()
-  pfUI.uf.pettarget:SetPoint("TOP", pfUI.uf.pet, "BOTTOM", 0, -default_border)
+  pfUI.uf.pettarget:SetPoint("TOP", pfUI.uf.pet or pfUI.uf.target or UIParent, "BOTTOM", 0, -default_border)
   UpdateMovable(pfUI.uf.pettarget)
 end)
