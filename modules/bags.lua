b38b29503448df42e7e5a016ae1f67f30f2e13b4
shagu <ericmauser@googlemail.com>
2018-06-08 17:27:39 +0200

    bags: add right-click trigger for auction house

--- modules/bags.lua
+++ modules/bags.lua
@@ -54,6 +54,19 @@ pfUI:RegisterModule("bags", function ()
       if CursorHasItem() then
         ClearCursor()
       end
+    elseif AuctionFrame and AuctionFrame:IsShown() and
+        AuctionFrameBrowse and AuctionFrameBrowse:IsShown() then
+      local link = GetContainerItemLink(bag,slot)
+      local name = link and string.sub(link, string.find(link, "%[")+1, string.find(link, "%]")-1) or ""
+      BrowseName:SetText(name)
+      AuctionFrameBrowse_Search()
+    elseif AuctionFrame and AuctionFrame:IsShown() and
+        AuctionFrameAuctions and AuctionFrameAuctions:IsShown() then
+      PickupContainerItem(bag,slot)
+      AuctionsItemButton:Click()
+      if CursorHasItem() then
+        ClearCursor()
+      end
     else
       pfHookUseContainerItem(bag,slot)
     end
