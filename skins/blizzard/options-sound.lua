8a3ab2026f38e43f6ded063060d8e4741d17af35
Artur Morozov <Artur91425@gmail.com>
2019-03-04 18:27:16 +0300

    skin: add sound options

---
+++ skins/blizzard/options-sound.lua
@@ -0,0 +1,32 @@
+pfUI:RegisterSkin("Options-Sound", function ()
+  local border = tonumber(pfUI_config.appearance.border.default)
+  local bpad = border > 1 and border - 1 or 1
+
+  StripTextures(SoundOptionsFrame)
+  CreateBackdrop(SoundOptionsFrame, nil, true, .75)
+  EnableMovable(SoundOptionsFrame)
+
+  local SoundOptionsFrameHeaderText = GetNoNameObject(SoundOptionsFrame, "FontString", "ARTWORK", SOUNDOPTIONS_MENU)
+  SoundOptionsFrameHeaderText:ClearAllPoints()
+  SoundOptionsFrameHeaderText:SetPoint("TOP", 0, -10)
+
+  SkinButton(SoundOptionsFrameDefaults)
+  SkinButton(SoundOptionsFrameCancel)
+  SkinButton(SoundOptionsFrameOkay)
+  SoundOptionsFrameOkay:ClearAllPoints()
+  SoundOptionsFrameOkay:SetPoint("RIGHT", SoundOptionsFrameCancel, "LEFT", -2*bpad, 0)
+
+  for i=1, 8 do
+    if i == 3 then i = 4 end -- Blizzard missed this number...
+    SkinCheckbox(_G["SoundOptionsFrameCheckButton"..i], 28)
+  end
+
+  SkinSlider(SoundOptionsFrameSlider1)
+  SoundOptionsFrameSlider1:ClearAllPoints()
+  SoundOptionsFrameSlider1:SetPoint("TOPRIGHT", SoundOptionsFrame, "TOPRIGHT", -18, -43)
+  for i=2, 4 do
+    SkinSlider(_G["SoundOptionsFrameSlider"..i])
+    _G["SoundOptionsFrameSlider"..i]:ClearAllPoints()
+    _G["SoundOptionsFrameSlider"..i]:SetPoint("TOP", _G["SoundOptionsFrameSlider"..i-1], "BOTTOM", 0, -30)
+  end
+end)
