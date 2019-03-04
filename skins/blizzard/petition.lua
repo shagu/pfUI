66504615312a635f2688702adfb9ddf3324bdf0a
Artur Morozov <Artur91425@gmail.com>
2019-03-04 18:28:30 +0300

    skin: add PetitionFrame

---
+++ skins/blizzard/petition.lua
@@ -0,0 +1,39 @@
+pfUI:RegisterSkin("PetitionFrame", function ()
+  local border = tonumber(pfUI_config.appearance.border.default)
+  local bpad = border > 1 and border - 1 or 1
+
+  StripTextures(PetitionFrame)
+  CreateBackdrop(PetitionFrame, nil, nil, .75)
+  PetitionFrame.backdrop:SetPoint("TOPLEFT", 12, -16)
+  PetitionFrame.backdrop:SetPoint("BOTTOMRIGHT", -28, 66)
+  PetitionFrame:SetHitRectInsets(12,28,16,66)
+  EnableMovable(PetitionFrame)
+
+  SkinCloseButton(PetitionFrameCloseButton, PetitionFrame.backdrop, -6, -6)
+
+  PetitionNpcNameFrame:ClearAllPoints()
+  PetitionNpcNameFrame:SetPoint("TOP", PetitionFrame.backdrop, "TOP", 0, -10)
+
+  SkinButton(PetitionFrameSignButton)
+  SkinButton(PetitionFrameCancelButton)
+  SkinButton(PetitionFrameRenameButton)
+  PetitionFrameRenameButton:ClearAllPoints()
+  PetitionFrameRenameButton:SetWidth(100)
+  PetitionFrameRenameButton:SetPoint("RIGHT", PetitionFrameCancelButton, "LEFT", -2*bpad, 0)
+  SkinButton(PetitionFrameRequestButton)
+  PetitionFrameRequestButton:ClearAllPoints()
+  PetitionFrameRequestButton:SetPoint("RIGHT", PetitionFrameRenameButton, "LEFT", -2*bpad, 0)
+
+  local texture = "StationeryTest"
+  if date("%m%d") == "0223" then texture = "Stationery_Val" end
+  local tex_Left = PetitionFrame:CreateTexture("BACKGROUND")
+  tex_Left:SetTexture("Interface\\Stationery\\"..texture.."1")
+  tex_Left:SetPoint("TOPLEFT", 23, -81)
+  tex_Left:SetWidth(tex_Left:GetWidth() + 10)
+  tex_Left:SetHeight(330)
+  local tex_Right = PetitionFrame:CreateTexture("BACKGROUND")
+  tex_Right:SetTexture("Interface\\Stationery\\"..texture.."2")
+  tex_Right:SetPoint("LEFT", tex_Left, "RIGHT", 0, 0)
+  tex_Right:SetWidth(tex_Right:GetWidth() + 10)
+  tex_Right:SetHeight(330)
+end)
