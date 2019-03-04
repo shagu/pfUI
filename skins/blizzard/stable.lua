68b14daed86a5fdcd9da3255ccf37ad0a6e0ec76
Artur Morozov <Artur91425@gmail.com>
2019-03-04 17:55:52 +0300

    skin: add PetStableFrame

---
+++ skins/blizzard/stable.lua
@@ -0,0 +1,40 @@
+pfUI:RegisterSkin("PetStableFrame", function ()
+  StripTextures(PetStableFrame)
+  CreateBackdrop(PetStableFrame, nil, nil, .75)
+  PetStableFrame.backdrop:SetPoint("TOPLEFT", 10, -10)
+  PetStableFrame.backdrop:SetPoint("BOTTOMRIGHT", -32, 72)
+  PetStableFrame:SetHitRectInsets(10,32,10,72)
+  EnableMovable(PetStableFrame)
+
+  SkinCloseButton(PetStableFrameCloseButton, PetStableFrame.backdrop, -6, -6)
+
+  PetStableFrame:DisableDrawLayer("BACKGROUND")
+
+  PetStableTitleLabel:ClearAllPoints()
+  PetStableTitleLabel:SetPoint("TOP", PetStableFrame.backdrop, "TOP", 0, -10)
+
+  EnableClickRotate(PetStableModel)
+  PetStableModelRotateLeftButton:Hide()
+  PetStableModelRotateRightButton:Hide()
+
+  PetStablePetInfo:ClearAllPoints()
+  PetStablePetInfo:SetPoint("TOPLEFT", PetStableModel, "TOPLEFT", 10, 0)
+
+  local buttons = {"PetStableCurrentPet", "PetStableStabledPet1", "PetStableStabledPet2"}
+  for _,v in pairs(buttons) do
+    local button = _G[v]
+    StripTextures(button)
+    SkinButton(button, nil, nil, nil, _G[button:GetName().."IconTexture"])
+
+    button.Enable = function()
+      button:SetBackdropColor()
+      button.locked = false
+    end
+    button.Disable = function()
+      button:SetBackdropColor(1, .1, .1, .3)
+      button.locked = true
+    end
+  end
+
+  SkinButton(PetStablePurchaseButton)
+end)
