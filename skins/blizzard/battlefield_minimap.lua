11425e6e74c4e1882d99cadc75305a4ccadcb82c
Artur Morozov <Artur91425@gmail.com>
2019-03-04 17:50:10 +0300

    skin: add BattlefieldMinimap

---
+++ skins/blizzard/battlefield_minimap.lua
@@ -0,0 +1,22 @@
+pfUI:RegisterSkin("BattlefieldMinimap", function ()
+  local border = tonumber(pfUI_config.appearance.border.default)
+
+  HookAddonOrVariable("Blizzard_BattlefieldMinimap", function()
+    StripTextures(BattlefieldMinimap)
+    CreateBackdrop(BattlefieldMinimap, nil, nil, 0)
+
+    BattlefieldMinimap:SetWidth(220)
+    BattlefieldMinimap:SetHeight(146)
+
+    SkinCloseButton(BattlefieldMinimapCloseButton, BattlefieldMinimap, 0, 0)
+
+    SkinTab(BattlefieldMinimapTab)
+    BattlefieldMinimapTabText:ClearAllPoints()
+    BattlefieldMinimapTabText:SetPoint("CENTER", 0, 0)
+
+    hooksecurefunc("BattlefieldMinimap_ShowOpacity", function()
+      OpacityFrame:ClearAllPoints()
+      OpacityFrame:SetPoint("TOPRIGHT", "BattlefieldMinimap", "TOPLEFT", -2*border, 0)
+    end, 1)
+  end)
+end)
