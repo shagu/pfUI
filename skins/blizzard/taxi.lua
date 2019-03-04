4e90604c49c677723e056f71b0a3d16f151c5b54
Artur Morozov <Artur91425@gmail.com>
2019-03-04 18:29:09 +0300

    skin: add TaxiFrame

---
+++ skins/blizzard/taxi.lua
@@ -0,0 +1,15 @@
+pfUI:RegisterSkin("TaxiFrame", function ()
+  StripTextures(TaxiFrame)
+  CreateBackdrop(TaxiFrame, nil, nil, .75)
+  TaxiFrame.backdrop:SetPoint("TOPLEFT", 10, -10)
+  TaxiFrame.backdrop:SetPoint("BOTTOMRIGHT", -32, 72)
+  TaxiFrame:SetHitRectInsets(10,32,10,72)
+  EnableMovable(TaxiFrame)
+
+  SkinCloseButton(TaxiCloseButton, TaxiFrame.backdrop, -6, -6)
+
+  TaxiFrame:DisableDrawLayer("BACKGROUND")
+
+  TaxiMerchant:ClearAllPoints()
+  TaxiMerchant:SetPoint("TOP", TaxiFrame.backdrop, "TOP", 0, -10)
+end)
