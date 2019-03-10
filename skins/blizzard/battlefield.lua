98b73c666d1e890bc6e65ccb628f361840dec3ec
shagu <ericmauser@googlemail.com>
2019-03-10 13:27:38 +0100

    skin: fix battlefield description

--- skins/blizzard/battlefield.lua
+++ skins/blizzard/battlefield.lua
@@ -1,4 +1,4 @@
-pfUI:RegisterSkin("Battlefield Map", function ()
+pfUI:RegisterSkin("Battlefield", function ()
   local border = tonumber(pfUI_config.appearance.border.default)
   local bpad = border > 1 and border - 1 or 1
 
