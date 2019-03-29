f6273d0f28f02376bce3cbd3bb391e8b60eb7240
shagu <ericmauser@googlemail.com>
2019-03-29 14:14:05 +0100

    skin: adjust trade item buttons and highlight

--- skins/blizzard/trade.lua
+++ skins/blizzard/trade.lua
@@ -20,24 +20,28 @@ pfUI:RegisterSkin("Trade", function ()
 
   for i = 1, MAX_TRADE_ITEMS do
     local PlayerButton = _G["TradePlayerItem"..i]
-    StripTextures(PlayerButton)
-    CreateBackdrop(PlayerButton, nil, nil, .75)
     local PlayerItemButton = _G["TradePlayerItem"..i.."ItemButton"]
+    local RecipientButton = _G["TradeRecipientItem"..i]
+    local RecipientItemButton = _G["TradeRecipientItem"..i.."ItemButton"]
+
+    StripTextures(PlayerButton)
     StripTextures(PlayerItemButton)
     SkinButton(PlayerItemButton, nil, nil, nil, _G[PlayerItemButton:GetName()..'IconTexture'])
+    local PlayerItemButtonBG = PlayerButton:CreateTexture(nil, "LOW")
+    PlayerItemButtonBG:SetTexture(1,1,1,.05)
+    PlayerItemButtonBG:SetAllPoints()
 
-    local RecipientButton = _G["TradeRecipientItem"..i]
     StripTextures(RecipientButton)
-    CreateBackdrop(RecipientButton, nil, nil, .75)
-    local RecipientItemButton = _G["TradeRecipientItem"..i.."ItemButton"]
     StripTextures(RecipientItemButton)
-    SkinButton(RecipientItemButton)
+    SkinButton(RecipientItemButton, nil, nil, nil, _G[RecipientItemButton:GetName()..'IconTexture'])
+    local RecipientButtonBG = RecipientButton:CreateTexture(nil, "LOW")
+    RecipientButtonBG:SetTexture(1,1,1,.05)
+    RecipientButtonBG:SetAllPoints()
   end
 
   hooksecurefunc("TradeFrame_UpdateTargetItem", function(id)
     HandleIcon(_G["TradeRecipientItem"..id.."ItemButton"], _G["TradeRecipientItem"..id..'IconTexture'])
   end, 1)
-
 
   SkinMoneyInputFrame(TradePlayerInputMoneyFrame)
 
@@ -47,4 +51,25 @@ pfUI:RegisterSkin("Trade", function ()
   SkinButton(TradeFrameTradeButton)
   TradeFrameTradeButton:ClearAllPoints()
   TradeFrameTradeButton:SetPoint("RIGHT", TradeFrameCancelButton, "LEFT", -2*bpad, 0)
+
+  -- skin highlights
+  TradeHighlightPlayerTop:SetTexture(0, 0, 0, 0)
+  TradeHighlightPlayerMiddle:SetTexture(0, 0, 0, 0)
+  TradeHighlightPlayerBottom:SetTexture(0, 1, 0, 0.2)
+  TradeHighlightPlayerBottom:SetPoint("TOPLEFT", 2, 0)
+
+  TradeHighlightPlayerEnchantTop:SetTexture(0, 0, 0, 0)
+  TradeHighlightPlayerEnchantMiddle:SetTexture(0, 0, 0, 0)
+  TradeHighlightPlayerEnchantBottom:SetTexture(0, 1, 0, 0.2)
+  TradeHighlightPlayerEnchantBottom:SetPoint("TOPLEFT", 2, 0)
+
+  TradeHighlightRecipientTop:SetTexture(0, 0, 0, 0)
+  TradeHighlightRecipientMiddle:SetTexture(0, 0, 0, 0)
+  TradeHighlightRecipientBottom:SetTexture(0, 1, 0, 0.2)
+  TradeHighlightRecipientBottom:SetPoint("TOPLEFT", 2, 0)
+
+  TradeHighlightRecipientEnchantTop:SetTexture(0, 0, 0, 0)
+  TradeHighlightRecipientEnchantMiddle:SetTexture(0, 0, 0, 0)
+  TradeHighlightRecipientEnchantBottom:SetTexture(0, 1, 0, 0.2)
+  TradeHighlightRecipientEnchantBottom:SetPoint("TOPLEFT", 2, 0)
 end)
