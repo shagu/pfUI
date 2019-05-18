3a7536e1ca3704250e1117879937fd6c20a4b1ef
Artur91425 <Artur91425@gmail.com>
2019-05-18 22:18:51 +0300

    skins: add itemtext skin

---
+++ skins/blizzard/itemtext.lua
@@ -0,0 +1,86 @@
+pfUI:RegisterSkin("Books", function ()
+  StripTextures(ItemTextFrame)
+  CreateBackdrop(ItemTextFrame, nil, nil, .75)
+  ItemTextFrame.backdrop:SetPoint("TOPLEFT", 12, -12)
+  ItemTextFrame.backdrop:SetPoint("BOTTOMRIGHT", -30, 72)
+  ItemTextFrame:SetHitRectInsets(12,30,12,72)
+  EnableMovable(ItemTextFrame)
+
+  SkinCloseButton(ItemTextCloseButton, ItemTextFrame.backdrop, -6, -6)
+
+  ItemTextScrollFrame:SetWidth(292)
+  ItemTextPageText:SetWidth(282)
+  ItemTextPageText:ClearAllPoints()
+  ItemTextPageText:SetPoint("TOPLEFT", 4, -15)
+
+  ItemTextTitleText:ClearAllPoints()
+  ItemTextTitleText:SetPoint("TOP", ItemTextFrame.backdrop, "TOP", 0, -10)
+
+  StripTextures(ItemTextScrollFrame)
+  CreateBackdrop(ItemTextScrollFrame, nil, true, .75)
+  SkinScrollbar(ItemTextScrollFrameScrollBar)
+  ItemTextScrollFrame:ClearAllPoints()
+  ItemTextScrollFrame:SetPoint("TOPRIGHT", -66, -46)
+
+  local bg = ItemTextScrollFrame:CreateTexture(nil, "LOW")
+  bg:SetAllPoints()
+  bg:SetTexCoord(.1,1,0,1)
+  bg:SetTexture("Interface\\Stationery\\StationeryTest1")
+
+  ItemTextCurrentPage:ClearAllPoints()
+  ItemTextCurrentPage:SetPoint("TOP", ItemTextScrollFrame, "BOTTOM", 0, -10)
+  local orig_SetText = ItemTextCurrentPage.SetText
+  ItemTextCurrentPage.SetText = function(self, text)
+    text = format(PAGE_NUMBER, text)
+    orig_SetText(self, text)
+  end
+
+  ItemTextCurrentPage:SetFontObject("GameFontWhite")
+  SkinArrowButton(ItemTextPrevPageButton, "left", 18)
+  ItemTextPrevPageButton:ClearAllPoints()
+  ItemTextPrevPageButton:SetPoint("TOPLEFT", ItemTextScrollFrame, "BOTTOMLEFT", 0, -6)
+  SkinArrowButton(ItemTextNextPageButton, "right", 18)
+  ItemTextNextPageButton:ClearAllPoints()
+  ItemTextNextPageButton:SetPoint("TOPRIGHT", ItemTextScrollFrame, "BOTTOMRIGHT", 0, -6)
+
+  ItemTextNextPageButton.Show = function(self) self:Enable() end
+  ItemTextNextPageButton.Hide = function(self) self:Disable() end
+  ItemTextPrevPageButton.Show = function(self) self:Enable() end
+  ItemTextPrevPageButton.Hide = function(self) self:Disable() end
+
+  do -- do not hide the scrollbar
+    ItemTextScrollFrame:Show()
+    ItemTextScrollFrameScrollBar:Show()
+    ItemTextScrollFrameScrollBarScrollUpButton:Show()
+    ItemTextScrollFrameScrollBarScrollDownButton:Show()
+
+    ItemTextScrollFrame.Show = function(self) end
+    ItemTextScrollFrame.Hide = function(self) end
+    ItemTextScrollFrameScrollBar.Show = function(self) self.thumb:Show() end
+    ItemTextScrollFrameScrollBar.Hide = function(self) self.thumb:Hide() end
+    ItemTextScrollFrameScrollBarScrollUpButton.Show = function(self) if self:GetParent():GetValue() ~= 0 then self:Enable() end end
+    ItemTextScrollFrameScrollBarScrollUpButton.Hide = function(self) self:Disable() end
+    ItemTextScrollFrameScrollBarScrollDownButton.Show = function(self) self:Enable() end
+    ItemTextScrollFrameScrollBarScrollDownButton.Hide = function(self) self:Disable() end
+
+    local first
+    HookScript(ItemTextFrame, "OnShow", function()
+      if not first then -- it is necessary to update the scrollbar when you first open the frame
+        ItemTextScrollFrameScrollBar:Show()
+        ItemTextScrollFrameScrollBar:Hide()
+        ItemTextScrollFrameScrollBarScrollUpButton:Show()
+        ItemTextScrollFrameScrollBarScrollUpButton:Hide()
+        ItemTextScrollFrameScrollBarScrollDownButton:Show()
+        ItemTextScrollFrameScrollBarScrollDownButton:Hide()
+        first = true
+      end
+    end)
+  end
+
+  CreateBackdrop(ItemTextStatusBar, nil, true)
+  ItemTextStatusBar:DisableDrawLayer("OVERLAY")
+  ItemTextStatusBar:SetStatusBarTexture("Interface\\AddOns\\pfUI\\img\\bar")
+  ItemTextStatusBar:SetHeight(12)
+  ItemTextStatusBar:ClearAllPoints()
+  ItemTextStatusBar:SetPoint("BOTTOM", ItemTextScrollFrame, "BOTTOM", 0, 50)
+end)
