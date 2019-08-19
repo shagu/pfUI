f1f4fa4094c924c714923f8c5acd8ad7fcfcdfa1
shagu <ericmauser@googlemail.com>
2019-08-19 19:40:49 +0200

    combopoints: whitelist combopoints for tbc

--- modules/combopoints.lua
+++ modules/combopoints.lua
@@ -1,4 +1,4 @@
-pfUI:RegisterModule("combopoints", "vanilla", function ()
+pfUI:RegisterModule("combopoints", "vanilla:tbc", function ()
   -- Hide Blizzard combo point frame and unregister all events to prevent it from popping up again
   ComboFrame:Hide()
   ComboFrame:UnregisterAllEvents()
