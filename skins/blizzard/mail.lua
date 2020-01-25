e8023903c30ba4c893782715beb484bf16d77552
shagu <ericmauser@googlemail.com>
2020-01-25 14:52:34 +0100

    skins: adjust mailframe buttons

--- skins/blizzard/mail.lua
+++ skins/blizzard/mail.lua
@@ -163,8 +163,11 @@ pfUI:RegisterSkin("Mailbox", "vanilla:tbc", function ()
     InboxNextPageButton:ClearAllPoints()
     InboxNextPageButton:SetPoint("TOPRIGHT", InboxFrame.backdrop, "BOTTOMRIGHT", 0, -10)
 
-    SkinArrowButton(InboxPrevPageButton, "left", 18)
-    SkinArrowButton(InboxNextPageButton, "right", 18)
+    SkinArrowButton(InboxPrevPageButton, "left", 16)
+    SkinArrowButton(InboxNextPageButton, "right", 16)
+
+    InboxPrevPageButton:GetRegions():SetPoint("LEFT", InboxPrevPageButton, "RIGHT", 5, 0)
+    InboxNextPageButton:GetRegions():SetPoint("RIGHT", InboxNextPageButton, "LEFT", -5, 0)
   end
 
   do -- SendMailFrame
