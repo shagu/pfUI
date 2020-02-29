001646812c3ccf953a07101c024a389797019b6c
shagu <ericmauser@googlemail.com>
2020-02-29 17:50:35 +0100

    hoverbind: enable module for tbc

--- modules/hoverbind.lua
+++ modules/hoverbind.lua
@@ -1,4 +1,4 @@
-pfUI:RegisterModule("hoverbind", "vanilla", function ()
+pfUI:RegisterModule("hoverbind", "vanilla:tbc", function ()
   local keymap = {
     -- buttons to binding association
     ["pfActionBarMainButton"]     = "ACTIONBUTTON",
