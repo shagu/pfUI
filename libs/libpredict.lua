54da13458edddc2405c9eb5f0a3c69eb17bee7bb
gretchen <gretchenn@gmail.com>
2024-09-18 16:56:42 +0200

    [ fix ] group frame names are different than unit names

--- libs/libpredict.lua
+++ libs/libpredict.lua
@@ -528,6 +528,10 @@ function libpredict:getHoTTime(unit, spell)
 	if unit == UNKNOWNOBJECT or unit == UNKOWNBEING then
 		return
  	end
+  if unit == "Group0" then
+    unit = "Player" --The player is always Group0, but the game only knows Player
+  end
+  unit = string.gsub(unit, "Group", "Party") --pfui frames are called GroupX but the units are called PartyX
 	local dbUnit = HoTs[UnitName(unit)]
 	if dbUnit and dbUnit[spell] and (dbUnit[spell].start + dbUnit[spell].dur) > GetTime() then
 		return dbUnit[spell].start, dbUnit[spell].dur
