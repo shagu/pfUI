6adb2a06f9c9c3cda1cf000f7e3aba5fc39012a1
shikulja <gamemazahist@gmail.com>
2024-11-24 16:49:44 +0300

    skin: QH Menu
    - adds more skining.
    strange places:
    - cant apply SkinEditBox for "QuickHealConfig_NotificationChannelNameEditBox"
    - dropdowns not fully skined
    Im done.

--- skins/thirdparty-vanilla/Quick_Heal.lua
+++ skins/thirdparty-vanilla/Quick_Heal.lua
@@ -1,7 +1,6 @@
 pfUI:RegisterSkin("Quick Heal", "vanilla", function ()
   HookAddonOrVariable("QuickHealHealingBar", function()
-  -- To get more Glassic style:
-  -- comment CreateBackdrop / CreateBackdropShadow
+  -- To get more Glassic style comment CreateBackdrop
 
     StripTextures(QuickHealHealingBar)
     CreateBackdrop(QuickHealHealingBar)
@@ -50,5 +49,104 @@ pfUI:RegisterSkin("Quick Heal", "vanilla", function ()
     QuickHealHealingBarText:SetPoint("CENTER", QuickHealHealingBar, "CENTER", 0, 0)
 
     QuickHealOverhealStatus_Text:SetPoint("BOTTOM", QuickHealOverhealStatus, "TOP", 0, 0)
-  end)
+end)
+
+  pfUI.api.HookAddonOrVariable("QuickHeal", function()
+    local frames = {
+      QuickHealConfig,
+      QuickHeal_DownrankSlider
+      -- QuickHealConfig_GeneralOptionsFrame,
+      -- QuickHealConfig_MessagesAndNotificationFrame,
+      -- QuickHealConfig_HealingTargetFilterFrame,
+      -- MTListFrame,
+      -- MeleeDPSListFrame,
+    }
+    for _, frame in pairs(frames) do
+      StripTextures(frame)
+      CreateBackdrop(frame)
+      CreateBackdropShadow(frame)
+    end
+
+    local buttons = {
+      QuickHealConfig_CloseButton,
+      QuickHealConfig_DefaultButton,
+    }
+    for _, button in pairs(buttons) do
+      SkinButton(button)
+    end
+
+    local checkboxes = {
+      QuickHealConfig_CheckButtonNotificationWhisper,
+      QuickHealConfig_CheckButtonNotificationChannel,
+      QuickHealConfig_CheckButtonNotificationParty,
+      QuickHealConfig_CheckButtonNotificationRaid,
+      QuickHealConfig_CheckButtonDisplayHealingBar,
+      QuickHealConfig_CheckButtonMessageScreenCenter,
+      QuickHealConfig_CheckButtonMessageChatWindow,
+      QuickHealConfig_CheckButtonOverhealMessageScreenCenter,
+      QuickHealConfig_CheckButtonOverhealMessageCastingBar,
+      QuickHealConfig_CheckButtonOverhealMessagePlaySound,
+      QuickHealConfig_CheckButtonTargetPriority,
+      QuickHealConfig_CheckButtonQuickClick,
+      QuickHealConfig_FilterRaidGroup1CheckBox,
+      QuickHealConfig_FilterRaidGroup2CheckBox,
+      QuickHealConfig_FilterRaidGroup3CheckBox,
+      QuickHealConfig_FilterRaidGroup4CheckBox,
+      QuickHealConfig_FilterRaidGroup5CheckBox,
+      QuickHealConfig_FilterRaidGroup6CheckBox,
+      QuickHealConfig_FilterRaidGroup7CheckBox,
+      QuickHealConfig_FilterRaidGroup8CheckBox,
+    }
+    for _, checkbox in pairs(checkboxes) do
+      SkinCheckbox(checkbox)
+    end
+
+    local sliders = {
+      QuickHealConfig_RatioFullSlider,
+      QuickHealConfig_RatioHealthySlider,
+      QuickHealConfig_RatioForceselfSlider,
+      QuickHealConfig_PetPrioritySlider,
+      QuickHealDownrank_Slider_NH,
+      QuickHealDownrank_Slider_FH,
+    }
+    for _, slider in pairs(sliders) do
+      SkinSlider(slider)
+    end
+
+    --SkinEditBox ???
+    local editboxes = {
+      QuickHealConfig_NotificationChannelNameEditBox,
+    }
+    for _, editbox in pairs(editboxes) do
+      StripTextures(editbox)
+      CreateBackdrop(editbox)
+      CreateBackdropShadow(editbox)
+    end
+
+      QuickHealConfig_NotificationChannelNameEditBox:ClearAllPoints()
+      QuickHealConfig_NotificationChannelNameEditBox:SetWidth(133)
+      QuickHealConfig_NotificationChannelNameEditBox:SetHeight(22)
+      QuickHealConfig_NotificationChannelNameEditBox:SetPoint("TOPLEFT", QuickHealConfig, "TOPLEFT", 139, -110)
+
+    local dropdowns = {
+      QuickHealConfig_ComboBoxNotificationStyle,
+      QuickHealConfig_ComboBoxMessageConfigure,
+      QuickHealConfig_ComboBoxNotificationChannel,
+    }
+    for _, dropdown in pairs(dropdowns) do
+      SkinDropDown(dropdown)
+    end
+
+    for i = 1, 3 do
+        local tab = _G["QuickHealConfigTab" .. i]
+        if tab then
+            SkinTab(tab)
+            -- Adjust the position of each tab based on its index
+            if i > 1 then
+                local previousTab = _G["QuickHealConfigTab" .. (i - 1)]
+                tab:SetPoint("TOPLEFT", previousTab, "TOPRIGHT", 3, 0)
+            end
+          end
+       end
+    end)
 end)
