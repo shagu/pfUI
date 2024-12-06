ea27c3032d5a4212848495cc4eb55a642a8bcd2e
Jason Gibson <jmgibson81@gmail.com>
2024-12-06 15:49:42 -0700

    removed duplicate code

--- libs/libtotem.lua
+++ libs/libtotem.lua
@@ -85,11 +85,8 @@ local totems = {
 
 GetTotemInfo = function(id, totrecall)
   if not active[id] or not active[id].name then return end
-  if totrecall then
-    libtotem:Clean(id)
-    return nil
-  end
-  if active[id].start + active[id].duration - GetTime() < 0 then
+  
+  if (active[id].start + active[id].duration - GetTime() < 0) or totrecall then
     libtotem:Clean(id)
     return nil
   end
