b849964dd188be67e354fd593f846432a3288780
shagu <ericmauser@googlemail.com>
2025-06-01 23:42:32 +0200

    autoshift: add esES turtle wow mount translations

--- modules/autoshift.lua
+++ modules/autoshift.lua
@@ -20,6 +20,7 @@ pfUI:RegisterModule("autoshift", "vanilla", function ()
     "^速度提高(.+)%%",
     -- turtle-wow
     "speed based on", "Slow and steady...", "Riding",
+    "Lento y constante...", "Aumenta la velocidad según tu habilidad de Montar.",
     "根据您的骑行技能提高速度。", "根据骑术技能提高速度。", "又慢又稳......",
   }
 
