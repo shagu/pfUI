1604e44ba3575ce28386decd7452343e482cae9c
ben <bensisson1@gmail.com>
2025-06-07 07:47:55 -0700

    fix: handle 24-hour to 12-hour clock conversion edge cases. 00:00 and 12:00.

--- modules/turtle-wow.lua
+++ modules/turtle-wow.lua
@@ -146,12 +146,22 @@ pfUI:RegisterModule("turtle-wow", "vanilla", function ()
         if C.global.twentyfour == "0" then
           local zn, sn = " AM", " AM"
 
-          if zh > 12 then
+          if zh == 0 then
+            zh = 12
+            zn = " AM"
+          elseif zh == 12 then
+            zn = " PM"
+          elseif zh > 12 then
             zh = zh - 12
             zn = " PM"
           end
 
-          if sh > 12 then
+          if sh == 0 then
+            sh = 12
+            sn = " AM"
+          elseif sh == 12 then
+            sn = " PM"
+          elseif sh > 12 then
             sh = sh - 12
             sn = " PM"
           end
